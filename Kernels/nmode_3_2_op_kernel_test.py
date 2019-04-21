241fce4516f758f3993695d481d6265d55585b07
Alex Reustle <areustledev@gmail.com>
2019-04-21 12:56:35 -0400

    rename cuda files

--- Kernels/nmode_3_2_op_kernel_test.py
+++ Kernels/nmode_3_2_op_kernel_test.py
@@ -1,13 +1,16 @@
 import tensorflow as tf
+import os
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '1'
 
-test_module = tf.load_op_library('./nmode_3_2_op_kernel.so')
+class NModeTest(tf.test.TestCase):
+  def testNMode(self):
+    test_module = tf.load_op_library('./nmode_3_2_op_kernel.so')
+    with self.session():
+      result = test_module.n_mode32(
+          [[[1, 2], [3, 4]],[[1,2],[3,4]]],
+          [[1,2],[3,4]]).eval()
+      expected = [[[ 7., 10.], [15., 22.]], [[ 7., 10.], [15., 22.]]]
+      self.assertAllEqual(result, expected)
 
-with tf.Session(''):
-  result = test_module.n_mode32(
-      [[[1, 2], [3, 4]],[[1,2],[3,4]]],
-      [[1,2],[3,4]]).eval()
-  print(result)
-  result = test_module.n_mode32(
-      [[[1, 2], [3, 4]],[[1,2],[3,4]]],
-      [[1,2],[3,4]]).eval()
-  print(result)
+if __name__ == "__main__":
+  tf.test.main()
