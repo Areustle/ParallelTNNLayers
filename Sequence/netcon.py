52730450156a51ec76c035f71dbb756f53f81b8f
rabbanitw <49704573+rabbanitw@users.noreply.github.com>
2019-04-23 13:47:44 -0400

    Update netcon.py
    Added eisensum notation support.

--- Sequence/netcon.py
+++ Sequence/netcon.py
@@ -1,29 +1,31 @@
-"""Get optimal contraction sequence using netcon algorithm
+"""Faster identification of optimal contraction sequences for tensor networks
 
 Reference:
     R. N. C. Pfeifer, et al.: Phys. Rev. E 90, 033315 (2014)
+    10.1103/PhysRevE.90.033315
 """
-
-#Tensorflow adaptation
-#__author__ = "Satoshi MORITA <morita@issp.u-tokyo.ac.jp>"
+#Tensorflow adaptation of https://github.com/smorita/Tensordot/blob/master/netcon.py
+#original__author__ = "Satoshi MORITA <morita@issp.u-tokyo.ac.jp>"
 #__date__ = "24 March 2016"
+
+#new__author__ = "Tahseen Rabbani<trabbani@math.umd.edu"
 
 import tensorflow as tf
 import numpy as np
 import sys
 import logging
 import time
-#import config
+import config
 
-DIMS = []
+DIMS = {}
 
 class Tensor:
     """Strips down a tensor as a list of key properties for the purposes of netcon.
 
     Attributes:
         rpn: contraction sequence with reverse polish notation.
-        decomp: a representation of contracted tensors.
-        bonds: list of dimension of indices.
+        decomp: a list 
+        bonds: list of indices in Eisenstein notation
         is_new: a flag.
     """
 
@@ -39,37 +41,47 @@ class Tensor:
             self.rpn, list(self.bonds), self.cost, self.decomp, self.is_new)
 
 
-def netcon(tList, DIMS):
+def netcon(tn,bond_dims):
     """Find optimal contraction sequence.
 
     Args:
-        tList: List of tensors
-        DIMS: Global containing a complete list of index dimensions across all tensors. 
-
+        tn: Tensor network/list of tensors.
+        bonds: a list of bonds for each tensor, in Eisenstein notation. Where 
+        two tensors are to be contracted, the same subscript will be used. An example:
+        
+             i
+       j---A---B---m
+           |   |
+           k   l
+        If tensor order is [A, B]
+        bond_dims[0]=['ijk']
+        bond_dims[1]=['ilm']
+        Bond legs should be in the same order as the dimensions listed by get_shape()
+ 
     Return:
         rpn: Optimal contraction sequence with reverse polish notation.
         cost: Total contraction cost.
     """
     tensor_set = _init(tn)
 
-    n = len(tList)
-    xi_min = float(min(DIMS))
+    n = len(tensor_set)
+    xi_min = 1
     mu_cap = 1.0
     mu_old = 0.0
 
-    while len(tList)<1:
+    while len(tensor_set[-1])<1:
         logging.info("netcon: searching with mu_cap={0:.6e}".format(mu_cap))
         mu_next = sys.float_info.max
         for c in range(1,n):
             for d1 in range((c+1)/2):
                 d2 = c-d1-1
-                n1 = len(tList[d1])
-                n2 = len(tList[d2])
+                n1 = len(tensor_set[d1])
+                n2 = len(tensor_set[d2])
                 for i1 in range(n1):
                     i2_start = i1+1 if d1==d2 else 0
                     for i2 in range(i2_start, n2):
-                        t1 = tList[d1][i1]
-                        t2 = tList[d2][i2]
+                        t1 = tensor_set[d1][i1]
+                        t2 = tensor_set[d2][i2]
 
                         if _is_disjoint(t1,t2): continue
                         if _is_overlap(t1,t2): continue
@@ -82,7 +94,7 @@ def netcon(tList, DIMS):
                             t_new = _contract(t1,t2)
                             is_find = False
                             for i,t_old in enumerate(tensor_set[c]):
-                                if t_new.bit == t_old.bit:
+                                if set(t_new.decomp) == set(t_old.decomp):
                                     if t_new.cost < t_old.cost:
                                         tensor_set[c][i] = t_new
                                     is_find = True
@@ -96,27 +108,35 @@ def netcon(tList, DIMS):
         logging.debug("netcon: tensor_num=" +  str([ len(s) for s in tensor_set]))
 
     t_final = tensor_set[-1][0]
-    return t_final.rpn, t_final.cost
+    return t_final.rpn, t_final.cost 
 
 
-def _init(tn):
+def _init(tn, bonds):
     """Initialize a set of tensors from a tensor network."""
     tensor_set=[[] for t in tn]
+    global DIMS
+    i=0
     for t in tn:
         rpn = [t.name]
         decomp = [x for x in rpn if x != -1]
-        bonds=t.get_shape().as_list()
-        #bonds = frozenset(bonds)
+        bonds=bonds[i]
+        shape=t.get_shape().as_list()
+        j=0
+        for char in bonds:
+            DIMS[char]=shape[j] #Running list of index dimensions keyed by bond name
+            bondlist[j]=char
+            j+=1
         cost = 0.0
-        tensor_set[0].append(Tensor(rpn,decomp,bonds,cost))
+        tensor_set[0].append(Tensor(rpn,decomp,bondlist,shape,cost))
+        i+=1
     return tensor_set
 
 
 def _get_cost(t1,t2):
     """Get the cost of contraction of two tensors."""
     cost = 1.0
-    for b in (t1.bonds + t2.bonds):
-        cost *= b
+    for b in set(t1.bonds+t2.bonds)+:
+        cost *= DIMS[b] #Multiplies bond dimensions together.
     cost += t1.cost + t2.cost
     return cost
 
@@ -125,25 +145,20 @@ def _contract(t1,t2):
     """Return a contracted tensor"""
     assert (not _is_disjoint(t1,t2))
     rpn = t1.rpn + t2.rpn + [-1]
-    bit = t1.decomp ^ t2.decomp # XOR
-    bonds = frozenset(t1.bonds ^ t2.bonds)
+    decomp = (t1.decomp).symmetric_difference(t2.decomp) # XOR
+    bonds = set(t1.bonds).symmetric_difference(set(t2.bonds))
     cost = _get_cost(t1,t2)
     return Tensor(rpn,decomp,bonds,cost)
 
 
 def _is_disjoint(t1,t2):
     """Check if two tensors are disjoint."""
-    t1list=tf.session().run(t1.bonds)
-    t1list=np_to_list(t1list)
-
-    t2list=_np_to_list(t2.bonds)
-    t2list=_np_to_list(t2list)
-    return (t1list).isdisjoint(t2list)
+    return set(t1.bonds).isdisjoint(set(t2.bonds))
 
 
 def _is_overlap(t1,t2):
     """Check if two tensors have the same basic tensor."""
-    return not (t1.decomp).isdisjoint(t2.decomp)
+    return not set(t1.decomp).isdisjoint(set(t2.decomp))
 
 
 def _print_tset(tensor_set):
