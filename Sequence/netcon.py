ece284a2ac66871c7645e7e1d2ee1bbba5cd605d
rabbanitw <49704573+rabbanitw@users.noreply.github.com>
2019-05-10 20:39:20 -0400

    Update netcon.py
    Algorithm can now handle any generalized tensor operation.

--- Sequence/netcon.py
+++ Sequence/netcon.py
@@ -1,10 +1,15 @@
 """Faster identification of optimal contraction sequences for tensor networks
-
 Reference:
     R. N. C. Pfeifer, et al.: Phys. Rev. E 90, 033315 (2014)
     10.1103/PhysRevE.90.033315
+    
+Tensorial Neural Networks: Generalization of Neural Networks and Application to Model Compression
+    Jiahao Su, Jingling Li, Bobby Bhattacharjee, Furong Huang
+    arXiv:1805.10352
 """
-#Tensorflow adaptation of https://github.com/smorita/Tensordot/blob/master/netcon.py
+
+"""Tensorflow adaptation of https://github.com/smorita/Tensordot/blob/master/netcon.py 
+This allows for generalized tensor operations as described in """
 #original__author__ = "Satoshi MORITA <morita@issp.u-tokyo.ac.jp>"
 #__date__ = "24 March 2016"
 
@@ -16,14 +21,12 @@ import math
 import sys
 import logging
 import time
-import config
-import pdb
+#import config
 
 DIMS = {} #Dictionary containing dimension of bonds. (dimensions of indices)
 
 class Tensor:
     """Strips down a tensor as a list of key properties for the purposes of netcon.
-
     Attributes:
         rpn: contraction sequence with reverse polish notation.
         decomp: a list 
@@ -43,25 +46,68 @@ class Tensor:
             self.rpn, list(self.bonds), self.cost, self.decomp, self.is_new)
 
 
-def netcon(tn,bonds):
+def netcon(tn,bonds,costmodel=0):
     """Find optimal contraction sequence.
-
     Args:
         tn: Tensor network/list of tensors.
-        bonds: a list of bonds for each tensor, in Eisenstein notation. Where 
+        bonds: a list of bonds/legs for each tensor, in modified** (see below) Eisenstein notation. Where 
         two tensors are to be contracted, the same subscript will be used. An example:
         
-             i
-       j---A---B---m
-           |   |
-           k   l
-        If tensor order is [A, B]
-        bond_dims[0]=['ijk']
-        bond_dims[1]=['ilm']
         Bond legs should be in the same order as the dimensions listed by get_shape()
+        
+        This can handle generalized tensor operations. 
+        
+        **FORMATTING**
+        The bond/leg lists require sensitive formatting. Please read below. 
+        
+        -Leg names must be of type string. Use *lower-case* letter-number combinations, 
+        EXCEPT in the case of convolution and partial outer product (see below):
+        
+        -Bond legs should be in the same order as the dimensions listed by tf.get_shape()
+        
+        -For outer product, just use different leg names.
+        Example: [i0,j,k] & [k,l,m1] have no common indices, so an outer product is performed.
+        
+        -For convolution, append * to the end of leg name, use same leg name, but with different casing.
+        We assume here that convolution between i & j pads the new leg to dimension length of max(i,j).
+        USE LOWER CASE FOR THE LEG WITH HIGHER DIMENSION.
+        
+        Example: [a,b*,c] & [d,B*,e] will convolve between the b*/B* indices. 
+        Please note that this assumes the convolution for a particular leg is only between two tensors.
+        I.e., [a,b*,c] & [d,B*,e] & [f, b*, h] is NOT valid.
+       
+       -For partial outer product, append + to end of leg names, with upper and lower casing.
+        Example: [n,p+,q] & [r,P+,s] will perform a partial outer product between p+/P+ indices.
+       
+       -For contraction, just leave the leg names the same.
+        Example" [i0,j3,k] & [i0,l2,m] will contract between the i indices. 
+        
+        Example of a tensor network with multiple tensor operations.
+        
+        Here, we are contracting on bonds j between A & B, convolving on bonds j&J between A & C
+        and taking a partial outer product on bond k between C & D.
+        We would express the bonds as follows:
+        A_bonds = [j,q*]
+        B_bonds = [j,l,m]
+        C_bonds = [Q, k+]
+        D_bonds = [n, k+, p]
+           
+            q   Q
+          A---^---C
+          |       | 
+        j |       = k
+          |       |
+          B       D
+         / \     / \ 
+        l   m   n   p
+        
+    costmodel=0 # of floating point operations
+                 =1 cost of storing intermediate arrays (better for GPUs)
  
     Return:
-        rpn: Optimal contraction sequence with reverse polish notation.
+        rpn: Optimal operation sequence with reverse polish notation.
+        E.g.,  [B,A,C,-1,-1] means perform all shared operations between A & C and then perform
+                remaining shared operations with B.
         cost: Total contraction cost.
     """
     tensor_set = _init(tn,bonds)
@@ -86,7 +132,7 @@ def netcon(tn,bonds):
                         t1 = tensor_set[d1][i1]
                         t2 = tensor_set[d2][i2]
 
-                        if _is_disjoint(t1,t2): continue
+#                        if _is_disjoint(t1,t2): continue
                         if _is_overlap(t1,t2): continue
 
                         mu = _get_cost(t1,t2)
@@ -94,7 +140,7 @@ def netcon(tn,bonds):
 
                         if (mu > mu_cap) and (mu < mu_next): mu_next = mu
                         if (mu > mu_0) and (mu <= mu_cap):
-                            t_new = _contract(t1,t2)
+                            t_new = _operate(t1,t2,costmodel)
                             is_find = False
                             for i,t_old in enumerate(tensor_set[c]):
                                 if set(t_new.decomp) == set(t_old.decomp):
@@ -120,14 +166,14 @@ def _init(tn, bonds):
     global DIMS
     i=0
     for t in tn:
-        bondlist=[]
+        bondlist=[] 
         rpn = [t.name]
         decomp = [x for x in rpn if x != -1]
         shape=t.get_shape().as_list()
-        j=0
-        for char in bonds[i]:
-            DIMS[char]=shape[j] #Running list of index dimensions keyed by bond name
-            bondlist.append(char)
+        j=0 #Index of individual legs. 
+        for legname in bonds[i]: #Running list of index dimensions keyed by name.   
+            DIMS[legname]=shape[j] 
+            bondlist.append(legname)   
             j+=1
         cost = 0.0
         tensor_set[0].append(Tensor(rpn,decomp,bondlist,cost))
@@ -135,31 +181,65 @@ def _init(tn, bonds):
     return tensor_set
 
 
-def _get_cost(t1,t2):
-    """Get the cost of contraction of two tensors."""
+def _get_cost(t1,t2,costmodel=0):
+    """Get the cost of conducting multiple generalized operations on two tensors."""
     cost = 1.0
-    fullbonds=t1.bonds+t2.bonds
-    for b in set(fullbonds): #Remove duplicate bonds to express an eisen contract.
-        cost *= DIMS[b] #Multiplies bond dimensions together.
+    t1bonds=t1.bonds
+    t2bonds=t2.bonds
+    lowerbonds1= [k.lower() for k in t1bonds]
+    lowerbonds2= [k.lower() for k in t2bonds] #lower case everything to identify convolving/p.o.p. pairs
+    commonbonds=list(set(lowerbonds1).intersection(lowerbonds2)) #list out any common bonds.
+    
+    fullbonds=t1.bonds+t2.bonds #Full list of bonds.
+    bonds_with_conv = set(_conv_pop(fullbonds,False,True)) #Determines bonds after performing p.o.p's
+    
+    #contractions/pop's, but not convolution
+    
+    if len(commonbonds)==0: #No common bonds, so perform outer product
+        for b in fullbonds:
+            cost *=DIMS[b] #Here, cost model is irrelevant.
+        cost = cost + t1.cost + t2.cost 
+        return cost
+    if costmodel==0:
+        for b in bonds_with_conv:
+            cost *= DIMS[b] 
+            #Multiplies bond dimensions together. For convolutions, both leg dimensions are multiplied in.
+    elif costmodel==1:
+        nocontracts = set(t1bonds).symmetric_difference(set(t2bonds)) #Bonds with no contractions
+        bonds_with_no_pairs = set(_conv_pop(nocontracts,True,True,True)) #Bonds with no pairs at all. 
+        for b in bonds_with_no_pairs:
+            cost *= DIMS[b]
+#                cost *= DIMS[b] #Multiplies *unshared* bond dimensions together.
+#                #Since legs targeted for convolution/p.o.p. technically have different names, 
+#                #symmetric_difference will not remove them, so we just check for the appended operator.
     cost = cost + t1.cost + t2.cost
     return cost
 
 
-def _contract(t1,t2):
-    """Return a contracted tensor"""
-    assert (not _is_disjoint(t1,t2))
+def _operate(t1,t2,costmodel=0):
+    """Returns a modified tensor after all convolutions, outer products and contractions."""
+    #assert (not _is_disjoint(t1,t2))
     rpn = t1.rpn + t2.rpn + [-1]
+    
+    
     decomp = set(t1.decomp).symmetric_difference(set(t2.decomp)) # XOR
-    decomp = list(decomp)
-    bonds = set(t1.bonds).symmetric_difference(set(t2.bonds))
-    bonds = list(bonds)
-    cost = _get_cost(t1,t2)
-    return Tensor(rpn,decomp,bonds,cost)
+    decomp = list(decomp) #Indicate which initial tensors are involved.
+    
+    t1bonds = set(t1.bonds)
+    t2bonds = set(t2.bonds)
+    
+    fullbonds = (t1bonds).symmetric_difference(t2bonds) #First remove contracted pairs. 
+ 
+    newbonds = list(_conv_pop(fullbonds))
+    
+    cost = _get_cost(t1,t2,costmodel)
+    return Tensor(rpn,decomp,newbonds,cost)
 
-
-def _is_disjoint(t1,t2):
-    """Check if two tensors are disjoint."""
-    return set(t1.bonds).isdisjoint(set(t2.bonds))
+#DEPRECATED
+#If two tensors in a network have no leg in common, we'll perform  an outer product. 
+#def _is_disjoint(t1,t2):
+#    """Check if two tensors are disjoint."""
+#    return set(t1.bonds).isdisjoint(set(t2.bonds))
 
 
 def _is_overlap(t1,t2):
@@ -172,3 +252,33 @@ def _print_tset(tensor_set):
     for level in range(len(tensor_set)):
         for i,t in enumerate(tensor_set[level]):
             print(level,i,t)
+            
+#Performs partial outer product and convolution on a list of bonds.
+def _conv_pop(fullbonds,do_Conv=True,do_Pop=True,remove_all_pairs=False):
+    newbonds=[]
+    tempBonds = [x for x in fullbonds] #Cache the fullbonds list
+    #Now we handle convolutions and partial outer products
+    for legs in tempBonds:
+        
+        if not legs in fullbonds: continue #If we've removed this leg from fullbonds, we don't care.
+        
+        if legs[-1]=="*" and do_Conv:
+            if DIMS[legs]==DIMS[legs.lower()] and (legs.upper() in fullbonds): #Check for a convolved pair.
+                fullbonds.remove(legs.upper()) #Remove the convolving leg with lower dimension.
+                if remove_all_pairs:
+                    fullbonds.remove(legs) #Remove the entire convolution pai.r
+            elif legs.lower() in fullbonds:
+                fullbonds.remove(legs) #Else remove itself if it has the lower dimension
+                if remove_all_pairs:
+                    fullbonds.remove(legs(lower()))
+        elif legs[-1]=="+" and do_Pop:
+            if legs==legs.upper() and (legs.lower() in fullbonds): #Check for a partial o.p. pair
+                fullbonds.remove(legs.lower())
+                if remove_all_pairs:
+                    fullbonds.remove(legs) #Remove the entire partial outer product pair.
+            elif legs.upper() in fullbonds:
+                fullbonds.remove(legs.upper()) #Remove one of the o.p. pair 
+                if remove_all_pairs:
+                    fullbonds.remove(legs)
+    newbonds = list(fullbonds)
+    return newbonds
