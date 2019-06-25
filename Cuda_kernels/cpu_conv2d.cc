6b1019739046b1414ea84900888f4ae73b05238b
Alex Reustle <areustledev@gmail.com>
2019-06-25 08:12:57 -0400

    Add convolution utilities lib with padding function

--- Cuda_kernels/cpu_conv2d.cc
+++ Cuda_kernels/cpu_conv2d.cc
@@ -1,18 +1,18 @@
 #include "cpu_conv2d.h"
 
-void cudnn_imp::conv2d( float*       U, //[N,C,H,W]
-                        void*        K, //[C,F,KH,KW]
-                        float*       V, //[N,C,H,W]
-                        const size_t dN  = 1,
-                        const size_t dC  = 16,
-                        const size_t dH  = 32,
-                        const size_t dW  = 32,
-                        const size_t dF  = 16,
-                        const size_t dKH = 3,
-                        const size_t dKW = 3 ){
+void cpu_imp::conv2d(float*       U, //[N,C,H,W]
+            float*       K, //[C,F,KH,KW]
+            float*       V, //[N,C,H,W]
+            const size_t dN  ,
+            const size_t dC  ,
+            const size_t dH  ,
+            const size_t dW  ,
+            const size_t dF  ,
+            const size_t dKH ,
+            const size_t dKW ){
   for (int n=0; n<dN; ++n)
   for (int c=0; c<dC; ++c)
-  for (int h=0; H<dH; ++h)
+  for (int h=0; h<dH; ++h)
   for (int w=0; w<dW; ++w)
     V[n + c*dN + h*dN*dC + w*dN*dC*dH] = 0 ;
 
--- Cuda_kernels/cpu_conv2d.h
+++ Cuda_kernels/cpu_conv2d.h
@@ -1,12 +1,14 @@
 #include <cstddef>
 
-void conv2d( float*       U,
-             void*        K,
-             float*       V,
-             const size_t dN  = 1,
-             const size_t dC  = 16,
-             const size_t dH  = 32,
-             const size_t dW  = 32,
-             const size_t dF  = 16,
-             const size_t dKH = 3,
-             const size_t dKW = 3 );
+namespace cpu_imp {
+  void conv2d( float*       U,
+               float*       K,
+               float*       V,
+               const size_t dN  = 1,
+               const size_t dC  = 16,
+               const size_t dH  = 32,
+               const size_t dW  = 32,
+               const size_t dF  = 16,
+               const size_t dKH = 3,
+               const size_t dKW = 3 );
+}
