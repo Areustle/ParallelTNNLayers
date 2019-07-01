0c670647ef114aa931389bdc09fcd2b664130635
Alex Reustle <areustledev@gmail.com>
2019-07-01 18:55:08 -0400

    attempt all cuda

--- Cuda_kernels/Tensor.h
+++ Cuda_kernels/Tensor.h
@@ -1,10 +1,11 @@
 #ifndef TENSOR_H
 #define TENSOR_H
 
+#include "CudaAllocator.h"
 #include <cstddef>
 #include <memory>
 
-template<typename T, class A = std::allocator<T>>
+template<class A = CudaAllocator>
 class Tensor
 {
 private:
@@ -16,7 +17,7 @@ public:
   size_t H;
   size_t W;
   size_t len;
-  T*     data;
+  float*     data;
 
   Tensor(size_t N, size_t C, size_t H, size_t W);
 
@@ -26,23 +27,23 @@ public:
 
   ~Tensor() { std::allocator_traits<A>::deallocate(Alloc, data, len); }
 
-  T*     get() { return data; }
+  float*     get() { return data; }
   size_t size() { return len; }
 
 
   template<class U>
-  Tensor(Tensor<T, U> const& other)
+  Tensor(Tensor<U> const& other)
       : N(other.N)
       , C(other.C)
       , H(other.H)
       , W(other.W)
       , len(N * C * H * W) {
-    T* buf = std::allocator_traits<A>::allocate(Alloc, len);
-    data   = new (buf) T(*other.data);
+    float* buf = std::allocator_traits<A>::allocate(Alloc, len);
+    data   = new (buf) float(*other.data);
   }
 
   template<class U>
-  Tensor& operator=(Tensor<T, U> const& other) {
+  Tensor& operator=(Tensor<U> const& other) {
     if (this == &other)
       return *this;
     if (len != other.len) {
--- Cuda_kernels/test_convolution.cpp.cu
+++ Cuda_kernels/test_convolution.cpp.cu
@@ -23,18 +23,22 @@ TEST_CASE("Check cudnn full convolution") {
   size_t len_output = (dN * dC * dH * dW);
   size_t len_kernel = (dC * dF * dKH * dKW);
 
-  Tensor<float> cpu_input(dN, dC, dH, dW);
-  Tensor<float> cpu_output(cpu_input);
-  Tensor<float> cpu_kernel = Tensor<float>(dC, dF, dKH, dKW);
+  Tensor<> cpu_input(dN, dC, dH, dW);
+  Tensor<> cpu_output(cpu_input);
+  Tensor<> cpu_kernel = Tensor<>(dC, dF, dKH, dKW);
 
   random_fill(len_input, cpu_input.data);
   random_fill(len_output, cpu_output.data);
   random_fill(len_kernel, cpu_kernel.data);
 
-  Tensor<float, CudaAllocator> gpu_input(cpu_input);
-  Tensor<float, CudaAllocator> gpu_output(cpu_output);
-  Tensor<float, CudaAllocator> gpu_kernel = cpu_kernel;
+  Tensor<> gpu_input(cpu_input);
+  Tensor<> gpu_output(cpu_output);
+  Tensor<> gpu_kernel = cpu_kernel;
 
   CHECK(cpu_input.data[0] == gpu_input.data[0]);
   CHECK(cpu_input.data[0] == doctest::Approx(gpu_input.data[0]).epsilon(1e-3));
+
+  for (int i=0; i<cpu_input.len; ++i){
+    CHECK(cpu_input.data[i] == doctest::Approx(gpu_input.data[i]).epsilon(1e-3));
+  }
 }
