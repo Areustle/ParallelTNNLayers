39799dd4272b82c696bfc21835c8861dab585ac9
Alex Reustle <areustledev@gmail.com>
2019-07-01 20:14:03 -0400

    restructure Tensor class

--- Cuda_kernels/Tensor.h
+++ Cuda_kernels/Tensor.h
@@ -5,8 +5,7 @@
 #include <cstddef>
 #include <memory>
 
-template<class A = CudaAllocator>
-class Tensor
+template<class A = CudaAllocator> class Tensor
 {
 private:
   A Alloc;
@@ -17,7 +16,7 @@ public:
   size_t H;
   size_t W;
   size_t len;
-  float*     data;
+  float* data;
 
   Tensor(size_t N, size_t C, size_t H, size_t W);
 
@@ -25,35 +24,11 @@ public:
 
   Tensor& operator=(Tensor const& other);
 
-  ~Tensor() { std::allocator_traits<A>::deallocate(Alloc, data, len); }
+  ~Tensor();
 
-  float*     get() { return data; }
+  float* get() { return data; }
   size_t size() { return len; }
 
-
-  template<class U>
-  Tensor(Tensor<U> const& other)
-      : N(other.N)
-      , C(other.C)
-      , H(other.H)
-      , W(other.W)
-      , len(N * C * H * W) {
-    float* buf = std::allocator_traits<A>::allocate(Alloc, len);
-    data   = new (buf) float(*other.data);
-  }
-
-  template<class U>
-  Tensor& operator=(Tensor<U> const& other) {
-    if (this == &other)
-      return *this;
-    if (len != other.len) {
-      std::allocator_traits<A>::deallocate(Alloc, data, len);
-      len  = other.len;
-      data = std::allocator_traits<A>::allocate(Alloc, len);
-    }
-    std::copy(&other.data[0], &other.data[0] + other.len, &data[0]);
-    return *this;
-  }
 };
 
 #endif /* TENSOR_H */
