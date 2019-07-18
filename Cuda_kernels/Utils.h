cfb125ae2e97a6e5ed3d62b454fec95bd3b7e6dc
Alex Reustle <areustledev@gmail.com>
2019-07-18 16:35:34 -0400

    Added padNCHW utility

--- Cuda_kernels/Utils.h
+++ Cuda_kernels/Utils.h
@@ -6,8 +6,11 @@
 #include <cstddef>
 #include <initializer_list>
 
-Tensor random_fill(std::initializer_list<int> lst, float lo = -1.0, float hi = 1.0);
+Tensor
+random_fill(std::initializer_list<int> lst, float lo = -1.0, float hi = 1.0);
 
 Tensor cp4recom(Tensor A, Tensor B, Tensor C, Tensor D);
+
+Tensor padNCHW(Tensor, int);
 
 #endif /* CONV_UTILS_H */
--- Cuda_kernels/test/test_Conv.cpp.cu
+++ Cuda_kernels/test/test_Conv.cpp.cu
@@ -3,8 +3,8 @@
 #include "../Tensor.h"
 #include "../Utils.h"
 /* #include "../cp4Conv2d.h" */
-#include "../cudnnConv2d.h"
 #include "../conv.cuh"
+#include "../cudnnConv2d.h"
 #include "../manual.h"
 
 TEST_CASE("Convolution test") {
@@ -13,12 +13,12 @@ TEST_CASE("Convolution test") {
   /* auto K1    = random_fill({ 16, 6 }, -1, 1); */
   /* auto K2    = random_fill({ 3, 6 }, -1, 1); */
   /* auto K3    = random_fill({ 3, 6 }, -1, 1); */
-  auto U     = random_fill({ 1, 1, 32, 32 }, 0, 1);
-  auto K     = random_fill({ 1, 1, 3, 3 }, 0, 1);
+  auto U = random_fill({ 1, 1, 32, 32 }, 0, 1);
+  auto K = random_fill({ 1, 1, 3, 3 }, 0, 1);
   /* auto K     = cp4recom(K0, K1, K2, K3); */
   auto Cudnn = nn_conv2d(U, K);
   /* auto Hand  = conv2d_full_cpu(U, K); */
-  /* auto Full_gpu  = conv2d_full_gpu(U, K); */
+  auto Full_gpu = conv2d_full_gpu(U, K);
   /* auto Tvm   = static_cp4_conv2d(U, K1, K2, K3, K0); */
   /* auto HandCp4  = conv2d_cp4_cpu(U, K0, K1, K2, K3); */
 
@@ -26,9 +26,9 @@ TEST_CASE("Convolution test") {
   /* REQUIRE(Cudnn.size() == Hand.size()); */
   /* for (int i = 0; i < Cudnn.size(); ++i) */
   /*   REQUIRE(Cudnn[i] == doctest::Approx(Hand[i]).epsilon(1e-5)); */
-  /* REQUIRE(Cudnn.size() == Full_gpu.size()); */
-  /* for (int i = 0; i < Cudnn.size(); ++i) */
-  /*   REQUIRE(Cudnn[i] == doctest::Approx(Full_gpu[i]).epsilon(1e-5)); */
+  REQUIRE(Cudnn.size() == Full_gpu.size());
+  for (int i = 0; i < Cudnn.size(); ++i)
+    REQUIRE(Cudnn[i] == doctest::Approx(Full_gpu[i]).epsilon(1e-5));
 
   /* // TVM CP4 */
   /* REQUIRE(Cudnn.size() == Tvm.size()); */
