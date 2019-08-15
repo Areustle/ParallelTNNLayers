f8f04d5014aa4d53da3e5941ac1380fa2c6d0d73
Alex Reustle <areustledev@gmail.com>
2019-08-15 01:02:50 -0400

    New benchmarks for renest

--- Cuda_kernels/bench/bench_cp4Conv2d.sh
+++ Cuda_kernels/bench/bench_cp4Conv2d.sh
@@ -16,7 +16,7 @@ do
   $(./_build/Cuda_kernels/Tensor_Benchmark \
     ./_build/Cuda_kernels/CP4Conv2dForward \
     Cuda_kernels/bench/tensors_${prob}.txt \
-    Cuda_kernels/results/CP4Conv2dForward_results_${GPUName}_${prob}.txt \
+    Cuda_kernels/results/CP4Conv2dForward_results_${GPUName}_$(date +'%F')_${prob}.txt \
     ${deviceNum})
   echo "Done ($prob)"
 done
